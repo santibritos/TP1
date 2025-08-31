d8eec94450d9a7a9209b5ad8fb9ece2f09afaa5e
Joaquin <gustavo.chazarreta10@gmail.com>
2025-08-31 15:17:51 -0300

    Ejercicio_01_v2

--- app/src/main/java/com/utn/frgp/tp1/Ejercicio1.java
+++ app/src/main/java/com/utn/frgp/tp1/Ejercicio1.java
@@ -1,6 +1,8 @@
 package com.utn.frgp.tp1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +23,21 @@ public class Ejercicio1 extends AppCompatActivity {
             return insets;
         });
     }
-    public int suma(int a, int b){
-        return a+b;
+    public void suma (View view){
+        EditText num1 = findViewById(R.id.txtN01);
+        EditText num2 = findViewById(R.id.txtN02);
+        EditText txtresultado = findViewById(R.id.txtResultado);
+
+        String valor1 = num1.getText().toString();
+        String valor2 = num2.getText().toString();
+
+        if (!valor1.isEmpty() && !valor2.isEmpty()) {
+            int v_01 = Integer.parseInt(valor1);
+            int v_02 = Integer.parseInt(valor2);
+            int v_resultado = v_01 + v_02;
+            txtresultado.setText("Resultado: " + v_resultado);
+        } else {
+             txtresultado.setText("Por favor ingrese ambos números");
+        }
     }
 }
